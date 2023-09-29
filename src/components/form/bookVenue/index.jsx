7b1f47c6e88322d90dd1290c817c6b718f9e728f
Enirose <enirosehellum@gmail.com>
2023-09-29 14:36:48 +0200

    booking not allowed, redirect to login if not logged in

--- src/components/form/bookVenue/index.jsx
+++ src/components/form/bookVenue/index.jsx
@@ -4,6 +4,7 @@ import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css'
 import { useNavigate, useParams } from "react-router-dom";
 import { CreateBooking } from "../../../posts/create/createBooking";
+import { load } from "../../localStorage";
 
 export default function BookDateByCalendar ({maxGuests, price}) {
   const [checkInDate, setCheckInDate] = useState(null);
@@ -42,26 +43,6 @@ export default function BookDateByCalendar ({maxGuests, price}) {
   };
 
 
-
-    // const calculateTotalAmount = () => {
-    //     if (checkInDate && checkOutDate) {
-    //     // Validate that check-out date is after check-in date
-    //         if (checkOutDate > checkInDate) {
-    //             // Calculate the number of days between check-in and check-out dates
-    //             const timeDifference = checkOutDate - checkInDate;
-    //             const numberOfDays = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-
-    //             // Calculate the total amount based on the price per night
-    //             const totalPrice = numberOfDays * price * guests;
-    //             setTotalAmount(totalPrice);
-
-    //             // Update total guests
-    //             setTotalGuests(guests);
-    //         } else {
-    //             setError("Check-out date must be after check-in date.");
-    //         }
-    //     }
-    // };
     useEffect(() => {
     // Calculate total amount whenever check-in, check-out dates, or guests change
     if (checkInDate && checkOutDate) {
@@ -83,32 +64,40 @@ export default function BookDateByCalendar ({maxGuests, price}) {
     }
   }, [checkInDate, checkOutDate, guests, price])
 
+  const isLoggedIn = load('token')
 
-    const reserveVenue = async () => {
+
+  const reserveVenue = async () => {
+
+    if(!isLoggedIn) {
+      navigate('/login');
+      return;
+    }
+
         // Check if all required fields are filled
-        if (!checkInDate || !checkOutDate || guests < 1) {
-            setError("Please fill in all required fields.");
-            return;
-        }
+    if (!checkInDate || !checkOutDate || guests < 1) {
+      setError("Please fill in all required fields.");
+      return;
+    }
 
         // Create the booking object
-        const bookingData = {
-            dateFrom: checkInDate.toISOString(),
-            dateTo: checkOutDate.toISOString(),
-            guests,
-            venueId: id, 
-        };
+    const bookingData = {
+        dateFrom: checkInDate.toISOString(),
+        dateTo: checkOutDate.toISOString(),
+        guests,
+        venueId: id, 
+    };
 
         // Call the API to create the booking
-        const bookingResult = await CreateBooking (bookingData);
+    const bookingResult = await CreateBooking (bookingData);
 
-        if (bookingResult) {
-            alert("Reservation successful!");
-            navigate('/profile')
-        } else {
-            setError("Reservation failed. Please try again later.");
-        }
-    };
+    if (bookingResult) {
+        alert("Reservation successful!");
+        navigate('/profile')
+    } else {
+        setError("Reservation failed. Please try again later.");
+    }
+  };
 
 
   return (
